12d74dad3d9707f46e7bf5c6b2c4e7227d7be337
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-10-07 14:14:22 +0000

    :shipit:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { IoIosCall } from "react-icons/io";
+
+import brjann from "./images/brjansi.jpeg";
+import { useEffect, useState } from "react";
+import sound from './sounds/saellEddu.mp3'
+const useAudio = url => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+      playing ? audio.play() : audio.pause();
+    },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+
 
 function App() {
+
+  let audio = new Audio(sound)
+  useEffect(() => console.log(audio))
+
+  const start = () => {
+    audio.play()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="brjansi-container">
+        <img className="brjann-img" src={brjann} alt="" />
+      </div>
+      <div className="call-container">
+        <IoIosCall onClick={() => start()} className="call-button"/>
+      </div>
     </div>
   );
 }
