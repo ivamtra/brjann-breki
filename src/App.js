e5748cef86ae00edc273b454cd8fc67ad750656d
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-10-08 21:09:28 +0000

    heyheyhey

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { MdCallEnd } from "react-icons/md";
 import brjann from "./images/brjansi.jpeg";
 import { useEffect, useState } from "react";
 import sound from "./sounds/saellEddu.mp3";
+import heyheyhey from './sounds/heyheyhey.mp3'
 const useAudio = (url) => {
   const [audio] = useState(new Audio(url));
   const [playing, setPlaying] = useState(false);
@@ -27,14 +28,17 @@ const useAudio = (url) => {
 
 function App() {
   let audio = new Audio(sound);
+
+  let merzedes = new Audio(heyheyhey)
   useEffect(() => console.log(audio));
 
   const start = () => {
-    audio.play();
+    merzedes.play();
   };
   const hangUp = () => {
     audio.pause();
   };
+
 
   return (
     <div className="background">
